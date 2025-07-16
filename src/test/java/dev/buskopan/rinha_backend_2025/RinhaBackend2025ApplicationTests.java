2c69997ed6fe355eb21151be9588a8b4c1b9f013
= <dmastergame74@gmail.com>
2025-07-16 14:21:33 -0300

    initial commit

---
+++ src/test/java/dev/buskopan/rinha_backend_2025/RinhaBackend2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.buskopan.rinha_backend_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackend2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
